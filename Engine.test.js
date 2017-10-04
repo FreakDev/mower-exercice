07ea9e1c8ad0f40e6916a8b02cfeaa78b9cfebaf
Mathias Desloges <freakdev@gmail.com>
2017-10-04 23:53:13 +0200

    add test for move

--- Engine.test.js
+++ Engine.test.js
@@ -3,12 +3,13 @@ var Engine = require('./Engine')
 
 var tests = [
     ['test constructor', function constructorTest() {
-
+        var e;
         e = new Engine('1 2 N', 'GAGAGAGAA', 5, 5)
         assert.equal(e.position.x, 1);
         assert.equal(e.position.y, 2);
         assert.equal(e.orientation, 0);
         assert.equal(e.ordersString, 'GAGAGAGAA');
+        assert.equal(e.orderIndex, 0);
         assert.equal(e.limits.x.min, 0);
         assert.equal(e.limits.x.max, 5);
         assert.equal(e.limits.y.min, 0);
@@ -30,6 +31,63 @@ var tests = [
         assert.equal(e.orientation, 2);
 
         e = new Engine('14 7 E', 'DGADGAGDA', 20, 12)
+        assert.equal(e.orientation, 3);
+    }],
+    ['execute one order', function tickTest() {
+        var e;
+        e = new Engine('1 2 N', 'A', 5, 5)
+        e.tick()
+        assert.equal(e.orderIndex, 1)
+        assert.equal(e.position.x, 1);
+        assert.equal(e.position.y, 3);
+        assert.equal(e.orientation, 0);
+
+        e = new Engine('1 3 N', 'G', 5, 5)
+        e.tick()
+        assert.equal(e.position.x, 1);
+        assert.equal(e.position.y, 3);
+        assert.equal(e.orientation, 1);
+
+        e = new Engine('1 3 W', 'A', 5, 5)        
+        e.tick()
+        assert.equal(e.position.x, 0);
+        assert.equal(e.position.y, 3);
+        assert.equal(e.orientation, 1);
+
+        e = new Engine('0 3 W', 'G', 5, 5)        
+        e.tick()
+        assert.equal(e.position.x, 0);
+        assert.equal(e.position.y, 3);
+        assert.equal(e.orientation, 2);
+
+        e = new Engine('0 3 S', 'A', 5, 5)        
+        e.tick()
+        assert.equal(e.position.x, 0);
+        assert.equal(e.position.y, 2);
+        assert.equal(e.orientation, 2);
+
+        e = new Engine('0 2 S', 'G', 5, 5)        
+        e.tick()
+        assert.equal(e.position.x, 0);
+        assert.equal(e.position.y, 2);
+        assert.equal(e.orientation, 3);
+
+        e = new Engine('0 2 E', 'A', 5, 5)        
+        e.tick()
+        assert.equal(e.position.x, 1);
+        assert.equal(e.position.y, 2);
+        assert.equal(e.orientation, 3);
+
+        e = new Engine('0 2 E', 'G', 5, 5)
+        e.tick()
+        assert.equal(e.position.x, 1);
+        assert.equal(e.position.y, 2);
+        assert.equal(e.orientation, 0);
+
+        e = new Engine('1 2 N', 'D', 5, 5)
+        e.tick()
+        assert.equal(e.position.x, 1);
+        assert.equal(e.position.y, 2);
         assert.equal(e.orientation, 3);
 
     }]
